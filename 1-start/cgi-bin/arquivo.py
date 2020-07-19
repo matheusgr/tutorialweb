59f336618b76cab3c7edd14d4458e3ad46d49d64
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2020-07-19 12:12:19 -0300

    Versão final pós-apresentação.

--- 1-start/cgi-bin/arquivo.py
+++ 1-start/cgi-bin/arquivo.py
@@ -1,8 +1,18 @@
 import cgi
+
 print("Content-Type: text/html")
+
+arq = open('contador', 'r')
+id = arq.read()
+arq.close()
 
 form = cgi.FieldStorage() 
 
 print()
 print("<h1>Olah!</h1>")
 print(form.getvalue("nome"))
+print("chamada... " + id)
+
+arq = open('contador', 'w')
+arq.write(str(int(id) + 1))
+arq.close()
